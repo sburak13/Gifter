a8701797459aa97a2d3edee59b9bfb76adfe9e93
Samantha Burak <samantha.burak@gmail.com>
2021-07-22 12:26:06 -0400

    Fixed images not hiding

--- Gifter/Views/GiftBasketCell.m
+++ Gifter/Views/GiftBasketCell.m
@@ -42,34 +42,57 @@
     if (numItems >= 3) {
         Gift *gift3 = [giftBasket.gifts objectAtIndex:2];
         self.description3.text = gift3.descrip;
-        self.description3.hidden = NO;
         self.image3.image = gift3.image;
-        self.image3.hidden = NO;
     }
     
     if (numItems >= 4) {
         Gift *gift4 = [giftBasket.gifts objectAtIndex:3];
         self.description4.text = gift4.descrip;
-        self.description4.hidden = NO;
         self.image4.image = gift4.image;
-        self.image4.hidden = NO;
     }
     
     if (numItems >= 5) {
         Gift *gift5 = [giftBasket.gifts objectAtIndex:4];
         self.description5.text = gift5.descrip;
-        self.description5.hidden = NO;
         self.image5.image = gift5.image;
-        self.image5.hidden = NO;
     }
     
-    /*
-    self.descriptionLabel.text = gift.descrip;
-    NSString *priceString = [gift.price stringValue];
-    self.priceLabel.text = priceString;
-    
-    self.giftImageView.image = gift.image;
-     */
+    [self hideUnhideUI:numItems];
+}
+
+- (void) hideUnhideUI:(int)numItems {
+    if (numItems == 2) {
+        self.description3.hidden = YES;
+        self.description4.hidden = YES;
+        self.description5.hidden = YES;
+        self.image3.hidden = YES;
+        self.image4.hidden = YES;
+        self.image5.hidden = YES;
+        
+    } else if (numItems == 3) {
+        self.description3.hidden = NO;
+        self.description4.hidden = YES;
+        self.description5.hidden = YES;
+        self.image3.hidden = NO;
+        self.image4.hidden = YES;
+        self.image5.hidden = YES;
+        
+    } else if (numItems == 4) {
+        self.description3.hidden = NO;
+        self.description4.hidden = NO;
+        self.description5.hidden = YES;
+        self.image3.hidden = NO;
+        self.image4.hidden = NO;
+        self.image5.hidden = YES;
+        
+    } else if (numItems == 5) {
+        self.description3.hidden = NO;
+        self.description4.hidden = NO;
+        self.description5.hidden = NO;
+        self.image3.hidden = NO;
+        self.image4.hidden = NO;
+        self.image5.hidden = NO;
+    }
 }
 
 @end
